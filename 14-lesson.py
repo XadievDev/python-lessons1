a1d8941c8f6debb6b4f25d1a3db671eb92619fd2
amirbek <amirbek007xadiev@gmail.com>
2022-10-22 12:02:09 +0500

    Add file

--- 14-lesson.py
+++ 14-lesson.py
@@ -5,3 +5,24 @@
 
 Author: Xadiev_dev
 """
+#
+# otam = {'ism':'Zavqi','t_yil':1975,'t_joyi':'Kogon'}
+# onam = {'ism':'Surayyo','t_yil':1980,'t_joyi':'Kogon'}
+# akam = {'ism':'Alisher','t_yil':1998,'t_joyi':'Kogon'}
+# print(f"Otamning ismi {otam['ism'].title()}, {otam['t_yil']}-yilda {otam['t_joyi'].title()} shahrida tug'ilgan.")
+# print(f"Onamning ismi {onam['ism'].title()}, {onam['t_yil']}-yilda {onam['t_joyi'].title()} shahrida tug'ilgan.")
+#
+#
+# defination = {
+#     'if':'agar',
+#     'else':'aks holda',
+#     'elif':'aks holda agar',
+#     'integer':'butun son',
+#     'float':"o'nlik son",
+#     'boolen':'mantiqiy',
+#     'for':'uchun',
+#     'not':'..mas',
+#     'dictionary':"lug'at"
+#     }
+# key = input().lower()
+# print(defination.get(key,'Bunday so\'z mavjud emas.'))

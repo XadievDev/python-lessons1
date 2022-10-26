cc42671a3d0e02fac3d3c32c16710f7186e26476
amirbek <amirbek007xadiev@gmail.com>
2022-10-26 11:27:55 +0500

    Add file

---
+++ 19-lesson.py
@@ -0,0 +1,25 @@
+"""
+25.10.22
+
+19-lesson: Functions
+
+Author: Xadiev_dev
+"""
+
+# def tug_yil_hisobla(ism,yosh):
+#     print(f"{ism.title()} {2022-yosh}-yil.")
+#
+# tug_yil_hisobla('amirbek',16)
+
+# def kv_kub(son):
+#     print(f"{son}ning kvadrati {son**2}ga, kubi esa {son**3}ga teng")
+#
+# kv_kub(5)
+#
+# def juft_toq(son):
+#     if son%2 == 0:
+#         print('Juft')
+#     else:
+#         print('Toq')
+# juft_toq(4)
+

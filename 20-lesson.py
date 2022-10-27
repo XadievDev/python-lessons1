4f782170a30d7470e8364b93c7bd909ed8f23940
amirbek <amirbek007xadiev@gmail.com>
2022-10-27 18:02:26 +0500

    Add file

---
+++ 20-lesson.py
@@ -0,0 +1,28 @@
+"""
+26.10.22
+
+19-lesson: Functions
+
+Author: Xadiev_dev
+"""
+
+# def malumot(ism,familiya,t_yil,t_joy,email='',tel=''):
+#     malumot = {
+#         'ism':ism,
+#         'familiya':familiya,
+#         't_yil':t_yil,
+#         't_joy':t_joy,
+#         'email':email,
+#         'tel':tel}
+#     return malumot
+# shaxs1 = malumot('amirbek','xadiev',2006,'kagan','amirbek007xadiev@gmail.com')
+# print(shaxs1)
+
+# def katta(a,b,c):
+#     if a>b and a>c:
+#         return a
+#     elif b>c:
+#         return b
+#     else:
+#         return c
+
